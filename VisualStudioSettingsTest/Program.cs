164e9c95f914c6c546c4c433ada0dd22abd3d8e4
Skybladev2 <qlzp2@mail.ru>
2016-12-22 17:07:52 +0300

    Added a sample project

---
+++ VisualStudioSettingsTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualStudioSettingsTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var devenvPath = @"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\devenv.exe";
+            var settings = ExternalSettingsManager.CreateForApplication(devenvPath);
+            Console.ReadKey();
+        }
+    }
+}
